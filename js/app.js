8cd209ba59eb1965968f7a769f27968f3352d239
David Ebinger <David Ebinger>
2016-09-27 19:26:57 -1000

    Review5

--- js/app.js
+++ js/app.js
@@ -43,11 +43,19 @@ console.log(drinksGin);
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
 
+var completedCodingChallenge = null;
+var traveledToSpace = null;
+console.log(completedCodingChallenge,traveledToSpace);
+  
 /*5.  Variables with Array for its value.
 Declare a variable named japanPrefectures and assign the value of any 5 prefectures in Japan.
 Console log the variable 
 Console log the value at index 1.
 Console log the value at index 3.*/
+
+var japanPrefectures = ["Tokyo","Shinjuku","Osaka","Kyoto","Nara"];
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
 
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
